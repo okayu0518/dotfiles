d71bf68831a8458449b010a4ac96ffd42009cac8
a <250376s@ugs.kochi-tech.ac.jp>
2025-01-07 22:12:58 +0900

    add vim settings

---
+++ .vimrc
@@ -0,0 +1,1 @@
+/home/okayu0518/.vimrc
--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 # 必要なツールのインストール（例えば、git, vim, zshなど）
 sudo apt update
-sudo apt install -y git evince curl wget \
+sudo apt install -y git curl wget \
   build-essential unzip vim python3 python3-pip \
   python3-venv nodejs npm ripgrep fzf xsel
 
 # シンボリックリンクの作成
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+mkdir -p ~/.vim/undo
 
 # 完了メッセージ
 echo "Dotfiles setup is complete! Please restart your terminal."
