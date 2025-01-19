420509e99fc0f60fec7b5931775d9c120d059e55
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-01-19 11:04:49 +0900

    rm lazygit;don't be lazygit add .git add .git add .!

--- install.sh
+++ install.sh
@@ -11,14 +11,7 @@ sudo apt update
 echo "Installing required tools for LazyVim..."
 sudo apt install -y git curl wget \
   build-essential unzip python3 python3-pip python3-venv \
-  nodejs npm ripgrep fzf fd-find xsel libfuse3 tmux htop ranger
-
-# lagygitのインストール
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
-rm lazygit.tar.gz lazygit
+  nodejs npm ripgrep fzf fd-find xsel libfuse3 tmux htop
 
 # # Dockerの設定
 # sudo apt install -y docker.io docker-compose
