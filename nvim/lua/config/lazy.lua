bd80d206d8967f68f85c9f87f0e88e39c8c5ecec
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-07-18 10:41:28 +0900

    tabtab coding

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -98,8 +98,6 @@ require("lazy").setup({
   {
     "github/copilot.vim",
     config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
     end,
   },
 
