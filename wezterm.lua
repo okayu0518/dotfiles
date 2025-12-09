273670641173afe80bac5d65999b60a8d2cf1d69
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-12-09 18:36:10 +0900

    add font config for wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,7 @@ local config = {}
 -- Wayland の IME バグ対策（重要）
 config.enable_wayland = false -- 一旦 Xwayland 経由だとかなり安定する
 config.audible_bell = "Disabled"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.75
 
 config.window_padding = {
   left = 0,
@@ -14,10 +14,22 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
-font = wezterm.font_with_fallback {
-	"JetBrains Mono",
-	"Noto Sans CJK JP",
-	"Noto Sans JP",
+
+config.font = wezterm.font_with_fallback {
+    {
+        -- 最も汎用的な「等幅」のエイリアス
+        family = "Liberation Mono", 
+        -- 合字（リガチャ）の無効化を適用
+        features = {
+            '-liga', 
+            '-calt', 
+        },
+    },
+
+    -- Noto Sans JPが確実にあれば、最も推奨されます。
+	  { family = "Noto Sans CJK JP" },
+    { family = "TakaoGothic" },
+    { family = "VL Gothic" },
 }
 
 return config
